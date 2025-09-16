1b53277c58eae54b365114e84452b38f61458367
JASM <jasm.319066@gmail.com>
2025-09-16 20:52:24 -0300

    Se agrego el tiempo
    El tiempo esta corriendo correctamente por ahora, ya se puede trabajar con él.

--- TamagochiProject/Assets/Scripts/ManagerUI.cs
+++ TamagochiProject/Assets/Scripts/ManagerUI.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class ManagerUI : MonoBehaviour
 {
+    public Estudiante estudiante;
+    public GameManager gameManager;
+
+    public TMP_Text textoHambre;
+    public TMP_Text textoSueno;
+    public TMP_Text textoDiversion;
+    public TMP_Text textoEstres;
+    public TMP_Text textoSocial;
+
+    public TMP_Text textoTiempo;
+
+    private string minuto;
+    private string hora;
+    private string dia;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +30,13 @@ public class ManagerUI : MonoBehaviour
     {
         
     }
+
+    public void mostrarTiempo()
+    {
+        minuto = gameManager.GetMinuto().ToString();
+        hora = gameManager.GetHora().ToString();
+        dia = gameManager.GetDia().ToString();
+        textoTiempo.text = $"Día: {gameManager.GetDia()+1} - {gameManager.GetHora():00}:{gameManager.GetMinuto():00}";
+    }
+
 }
