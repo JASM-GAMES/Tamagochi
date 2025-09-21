c5d49343afa1a4e99d70f9481a6e88f451f18edd
JASM <jasm.319066@gmail.com>
2025-09-20 22:31:48 -0300

    Esto es una prueba de git
    asdasdasdas

---
+++ TamagochiProject/Assets/Scripts/Prueba.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //asjdklajsldjlasjfl
+        Debug.Log("esta es una prueba");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
